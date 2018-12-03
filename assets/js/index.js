b272a1711c817c19a71cd22e1b48225ca67a79de
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2018-12-03 23:12:19 +0100

    Added firebase integration

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,31 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+	apiKey: "AIzaSyB8IZneKgocwhhpJW1e8wHY-mAxBMREAmo",
+	authDomain: "todolist-b1426.firebaseapp.com",
+	databaseURL: "https://todolist-b1426.firebaseio.com",
+	projectId: "todolist-b1426",
+	storageBucket: "todolist-b1426.appspot.com",
+	messagingSenderId: "138602765444"
+};
+
+firebase.initializeApp(config);
+var db = firebase.firestore();
+
+db.settings({
+	timestampsInSnapshots: true
+});
+
+// Get todo items from firebase on first load
+db.collection('items').get().then((snapshot) => {
+	snapshot.forEach((document) => {
+		$('ul').append(
+			`<li><span class="delete"><i class="fa fa-trash"></i></span> ${document.data().item} <span class="edit"><i class="fa fa-pencil-square-o"></i></span></li>`
+		);
+	});
+});
+
+
 // Toggle class of clicked LI to gray and strike out
 $('ul').on('click', 'li', function() {
 	$(this).toggleClass('selected');
@@ -26,6 +54,18 @@ $('input[type="text"').keypress(function(event){
 		let newItem = $(this).val();
 		$(this).val(""); // Empties the content of the input field
 		$('ul').append(`<li><span class="delete"><i class="fa fa-trash"></i></span> ${newItem} <span class="edit"><i class="fa fa-pencil-square-o"></i></span></li>`);
+		// Add data to firestore
+		db.collection('items').add({
+			item: newItem,
+			date: new Date().toLocaleDateString(),
+			time: new Date().toLocaleTimeString()
+		})
+		.then((docref) => {
+			console.log('Data written successfully');
+		})
+		.catch((error) => {
+			console.log(`Error writing data ${error}`);
+		})
 	}
 });
 
